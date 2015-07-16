b092839e91c5f6b8c05e535e549e194323eba90b
ALEXANDER PLETZER <axp5530@psu.edu>
2015-07-16 10:43:04 -0400

    example of contouring

--- python/readContour.py
+++ python/readContour.py
@@ -8,28 +8,54 @@ reader = vtk.vtkDICOMImageReader()
 reader.SetDirectoryName(dir)
 reader.Update()
 
+
+
 # vtkImageData
 imageData = reader.GetOutput()
+
+
 pointData = imageData.GetPointData()
 dataArray = pointData.GetArray(0)
 minVal, maxVal = dataArray.GetRange()
 
-print dataArray
-print 'minVal, maxVal = ', minVal, maxVal 
+xmin, xmax, ymin, ymax, zmin, zmax = imageData.GetBounds()
+print 'xmin, xmax = ', xmin, xmax
+print 'ymin, ymax = ', ymin, ymax
+print 'zmin, zmax = ', zmin, zmax
+
+print 'minVal, maxVal = ', minVal, maxVal
+nx, ny, nz = imageData.GetDimensions()
+print 'nx, ny, nz = ', nx, ny, nz
+
+smooth = vtk.vtkImageGaussianSmooth()
+npx, npy, npz = int(0.01*nx), int(0.01*ny), int(0.05*nz)
+print 'npx, npy, npz = ', npx, npy, npz
+smooth.SetStandardDeviation(npx, npy, npz)
+#smooth.SetRadiusFactors(10, 10, 10)
+smooth.SetInputData(imageData)
+smooth.Update()
+
+print smooth.GetOutput()
+print smooth.GetOutput().GetPointData().GetArray(0).GetRange()
+
 
 contour = vtk.vtkContourFilter()
 contour.SetNumberOfContours(1)
-contour.SetValue(0, 0.5*(minVal + maxVal))
+contour.SetValue(0, 200.0) # 0.5*(minVal + maxVal))
 contour.SetArrayComponent(0) # scalar
 contour.ComputeNormalsOn()
 
-contour.SetInputConnection(reader.GetOutputPort())
+#contour.SetInputConnection(reader.GetOutputPort())
+#contour.SetInputConnection(smooth.GetOutputPort())
+contour.SetInputData(smooth.GetOutput())
 
 mapper = vtk.vtkPolyDataMapper()
+mapper.ScalarVisibilityOff()
 mapper.SetInputConnection(contour.GetOutputPort())
 
 actor = vtk.vtkActor()
 actor.SetMapper(mapper)
+actor.GetProperty().SetColor(222/256., 184/256., 135/256.)
 
 renWin = vtk.vtkRenderWindow()
 ren = vtk.vtkRenderer()
